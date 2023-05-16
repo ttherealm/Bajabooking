481bba1bb710497b55246148f1e5822bff5320f6
Sodiq Ismail <olayinkaismailsodiq@gmail.com>
2023-05-16 14:54:52 +0100

    java

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
     $(".book-now").click(function(){
-        alert("Welcome")
+        window.location.href = "https://mydukaan.io/baja596/products/1-day-trip";
+    })
+})
+
+$(document).ready(function () {
+    $(".book-noww").click(function () {
+        window.location.href = "https://mydukaan.io/baja596/products/2-day-trip";
+    })
+})
+
+$(document).ready(function () {
+    $(".book-nowww").click(function () {
+        window.location.href = "https://mydukaan.io/baja596/products/3-day-trip";
     })
 })
